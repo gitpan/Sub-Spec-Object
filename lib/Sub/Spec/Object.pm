6f4a161d3041545090472999b062ff2703880bef
Steven Haryanto <stevenharyanto@gmail.com>
2011-08-29 16:57:15 +0000

    Import of SHARYANTO/Sub-Spec-Object-0.01 from CPAN.
    gitpan-cpan-distribution: Sub-Spec-Object
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         SHARYANTO/Sub-Spec-Object-0.01.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

---
+++ lib/Sub/Spec/Object.pm
@@ -0,0 +1,90 @@
+package Sub::Spec::Object;
+
+use 5.010;
+use strict;
+use warnings;
+
+our $VERSION = '0.01'; # VERSION
+
+require Exporter;
+our @ISA = qw(Exporter);
+our @EXPORT = qw(ssres);
+
+sub ssres {
+    require Sub::Spec::Object::Response;
+    Sub::Spec::Object::Response->new(@_);
+}
+
+sub ssreq {
+    require Sub::Spec::Object::Request;
+    Sub::Spec::Object::Request->new(@_);
+}
+
+sub ssspec {
+    require Sub::Spec::Object::Spec;
+    Sub::Spec::Object::Spec->new(@_);
+}
+
+sub sssub {
+    require Sub::Spec::Object::Sub;
+    Sub::Spec::Object::Sub->new(@_);
+}
+
+1;
+# ABSTRACT: Object-oriented interface for sub/spec/request/response/etc
+
+
+__END__
+=pod
+
+=head1 NAME
+
+Sub::Spec::Object - Object-oriented interface for sub/spec/request/response/etc
+
+=head1 VERSION
+
+version 0.01
+
+=head1 SYNOPSIS
+
+ use Sub::Spec::Object;
+
+ my $ssres = ssres [200, "OK", [1, 2, 3]];
+ print $ssres->is_success, # 1
+       $ssres->status,     # 200
+       $ssres->message,    # "OK"
+       $ssres->result;     # [1, 2, 3]
+
+ # TODO: ssspec
+ # TODO: ssreq
+ # TODO: sssub
+
+=head1 DESCRIPTION
+
+L<Sub::Spec> works using pure data structures, but sometimes it's convenient to
+have an object-oriented interface for those data. This module provides just
+that.
+
+=head1 FUNCTIONS
+
+=head2 ssres $res => OBJECT
+
+Exported by default. A shortcut for Sub::Spec::Object::Response->new($res).
+
+=head1 SEE ALSO
+
+L<Sub::Spec>
+
+=head1 AUTHOR
+
+Steven Haryanto <stevenharyanto@gmail.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2011 by Steven Haryanto.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
---
+++ lib/Sub/Spec/Object/Response.pm
@@ -0,0 +1,119 @@
+package Sub::Spec::Object::Response;
+
+use 5.010;
+use strict;
+use warnings;
+
+our $VERSION = '0.01'; # VERSION
+
+require Exporter;
+our @ISA = qw(Exporter);
+our @EXPORT = qw(ssres);
+
+sub new {
+    my ($class, $res) = @_;
+    $res //= [0, "", undef];
+    my $obj = \$res;
+    bless $obj, $class;
+}
+
+sub status {
+    my ($self, $new) = @_;
+    if (defined $new) {
+        my $old = ${$self}->[0];
+        ${$self}->[0] = $new;
+        return $old;
+    }
+    ${$self}->[0];
+}
+
+sub message {
+    my ($self, $new) = @_;
+    if (defined $new) {
+        my $old = ${$self}->[1];
+        ${$self}->[1] = $new;
+        return $old;
+    }
+    ${$self}->[1];
+}
+
+sub result {
+    my ($self, $new) = @_;
+    if (defined $new) {
+        my $old = ${$self}->[2];
+        ${$self}->[2] = $new;
+        return $old;
+    }
+    ${$self}->[2];
+}
+
+sub is_success {
+    my ($self) = @_;
+    my $status = ${$self}->[0];
+    $status >= 200 && $status <= 299;
+}
+
+1;
+# ABSTRACT: Represent sub response
+
+
+__END__
+=pod
+
+=head1 NAME
+
+Sub::Spec::Object::Response - Represent sub response
+
+=head1 VERSION
+
+version 0.01
+
+=head1 SYNOPSIS
+
+ use Sub::Spec::Object::Response;
+
+ my $ssres = Sub::Spec::Object::Response->new([200, "OK", [1, 2, 3]]);
+ print $ssres->is_success, # 1
+       $ssres->status,     # 200
+       $ssres->message,    # "OK"
+       $ssres->result;     # [1, 2, 3]
+
+=head1 DESCRIPTION
+
+This class provides an object-oriented interface for sub response.
+
+=head1 METHODS
+
+=head2 new($res) => OBJECT
+
+Create a new object from $res response data.
+
+=head2 $ssres->status
+
+Get or set status.
+
+=head2 $ssres->message
+
+Get or set message.
+
+=head2 $ssres->is_success
+
+True if status is between 200-299.
+
+=head1 SEE ALSO
+
+L<Sub::Spec::Object>
+
+=head1 AUTHOR
+
+Steven Haryanto <stevenharyanto@gmail.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2011 by Steven Haryanto.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
